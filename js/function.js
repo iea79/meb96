129b6b3778a19f4853df33f01eb4c6b29f5cd542
Evgeny Ivanov <busforward@gmail.com>
2017-04-24 21:43:34 +0500

    scroll

--- js/function.js
+++ js/function.js
@@ -38,7 +38,38 @@ $(document).ready(function() {
 
     var isAnimating  = false;
 
-	$(window).bind('mousewheel', function(event) {
+	// $(window).bind('mousewheel', function(e) {
+ //        // e.preventDefault();
+ //        // e.stopPropagation();
+
+	//     var headerTop = $('.first__screen').height();
+	//     	nextSection = $('#sofas').offset().top;
+
+	// 	if( isAnimating ) {
+	// 	   return false;
+	// 	}
+
+	// 	if (e.originalEvent.wheelDelta > 0) {
+	// 	    isAnimating  = false;
+	// 	} else {
+	// 	    isAnimating  = true;
+	// 	    if ($(window).scrollTop() < headerTop) {	    	
+	// 	        $('html, body').animate({ scrollTop: nextSection }, 500, function(){
+	// 	            isAnimating  = false;
+	// 	        });
+	// 	    } else {
+	// 			isAnimating  = false;
+	// 	    }
+	// 	}
+
+	// });
+
+	var lastScrollTop = 0;
+	$(window).scroll(function(event){
+		var st = $(this).scrollTop();
+        // e.preventDefault();
+        // e.stopPropagation();
+
 	    var headerTop = $('.first__screen').height();
 	    	nextSection = $('#sofas').offset().top;
 
@@ -46,9 +77,8 @@ $(document).ready(function() {
 		   return false;
 		}
 
-		if (event.originalEvent.wheelDelta > 0) {
-		    isAnimating  = false;
-		} else {
+		if (st > lastScrollTop){
+		   // downscroll code
 		    isAnimating  = true;
 		    if ($(window).scrollTop() < headerTop) {	    	
 		        $('html, body').animate({ scrollTop: nextSection }, 500, function(){
@@ -57,8 +87,18 @@ $(document).ready(function() {
 		    } else {
 				isAnimating  = false;
 		    }
+		} else {
+		   // upscroll code
+		    // isAnimating  = true;
+		    // if ($(window).scrollTop() < headerTop) {	    	
+		    //     $('html, body').animate({ scrollTop: 0 }, 500, function(){
+		    //         isAnimating  = false;
+		    //     });
+		    // } else {
+				isAnimating  = false;
+		    // }
 		}
-
+		lastScrollTop = st;
 	});
     
 	$('.reccomend__more').on('click', function(event) {
